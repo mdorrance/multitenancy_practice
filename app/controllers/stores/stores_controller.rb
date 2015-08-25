87f535dbefe8ca83c893d5557cec771e4feda793
Michael Dorrance <dorrancemd@gmail.com>
2015-08-25 10:57:54 -0600

    add before_action for store_url exists

--- app/controllers/stores/stores_controller.rb
+++ app/controllers/stores/stores_controller.rb
@@ -1,7 +1,12 @@
 class Stores::StoresController < ApplicationController
+  before_action :store_not_found
   helper_method :current_store
-  
+
   def current_store
     @current_store ||= Store.find_by(url: params[:store])
+  end
+
+  def store_not_found
+    redirect_to root_path if current_store.nil?
   end
 end
